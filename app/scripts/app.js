c8945a30832b069eead951a8a60a0421ca6229f8
darizkipratama <dpratama@gmail.com>
2018-02-05 12:27:26 +0700

    developing new dashboard add jquery.color library

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -41,46 +41,13 @@ angular
                     name:'sbAdminApp',
                     files:[
                     'scripts/directives/header/header.js',
-                    //'scripts/directives/header/header-notification/header-notification.js',
                     'scripts/directives/sidebar/sidebar.js',
                     'scripts/directives/sidebar/sidebar-search/sidebar-search.js'
                     ]
-                })//,
-                // $ocLazyLoad.load(
-                // {
-                //    name:'toggle-switch',
-                //    files:["bower_components/angular-toggle-switch/angular-toggle-switch.min.js",
-                //           "bower_components/angular-toggle-switch/angular-toggle-switch.css"
-                //       ]
-                // }),
-                // $ocLazyLoad.load(
-                // {
-                //   name:'ngAnimate',
-                //   files:['bower_components/angular-animate/angular-animate.js']
-                // })
-                // $ocLazyLoad.load(
-                // {
-                //   name:'ngCookies',
-                //   files:['bower_components/angular-cookies/angular-cookies.js']
-                // })
-                // $ocLazyLoad.load(
-                // {
-                //   name:'ngResource',
-                //   files:['bower_components/angular-resource/angular-resource.js']
-                // })
-                // $ocLazyLoad.load(
-                // {
-                //   name:'ngSanitize',
-                //   files:['bower_components/angular-sanitize/angular-sanitize.js']
-                // })
-                // $ocLazyLoad.load(
-                // {
-                //   name:'ngTouch',
-                //   files:['bower_components/angular-touch/angular-touch.js']
-                // })
+                })
             }
         }
-    })
+      })
       .state('dashboard.home',{
         url:'/home',
         controller: 'MainCtrl',
